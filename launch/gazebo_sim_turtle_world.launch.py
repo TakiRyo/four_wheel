c38291c497a57d1b6a13307d44793df2a51c094f
TakiRyo <takiryo00614@gmail.com>
2025-04-07 23:51:56 +0200

    first comment

---
+++ launch/gazebo_sim_turtle_world.launch.py
@@ -0,0 +1,45 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    package_name='four_wheel' 
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # This will start Gazebo and load the world.    
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={'world': os.path.join(
+            get_package_share_directory(package_name), 'worlds', 'turtle_world.world')}.items()
+    )
+
+    # Run the spawner node from the gazebo_ros package.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot',
+                                   '-x', '0.5',
+                                   '-y', '0.5',
+                                   '-z', '0.05'],
+                        output='screen')
+
+
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity, 
+    ])
